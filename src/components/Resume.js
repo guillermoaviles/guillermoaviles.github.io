380e2ff1787c1a5025211b26b297c56a1a5b9774
username <gjaviles7@gmail.com>
2023-01-17 13:28:26 -0500

    workaround for resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,32 +1,17 @@
 import React from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import resumeSrc from "../media/resume.pdf";
+import resumeImg from "../media/resume.png";
 import "../styles/Resume.scss";
 
-const options = {
-  cMapUrl: 'cmaps/',
-  cMapPacked: true,
-  standardFontDataUrl: 'standard_fonts/',
-};
 
 function Resume () {
-  function removeTextLayerOffset() {
-    const textLayers = document.querySelectorAll(
-      ".react-pdf__Page__textContent"
-    );
-    textLayers.forEach((layer) => {
-      const { style } = layer;
-      style.top = "0";
-      style.left = "0";
-      style.transform = "";
-    });
-  }
+  
   return (
     <div className="Resume" id="Resume">
       <div className="Resume__pdf-container">
-        <Document className="resume-pdf" file={resumeSrc} options={options}>
-          <Page onLoadSuccess={removeTextLayerOffset} pageNumber={1} />
-        </Document>
+        <img className="resume-pdf" src={resumeImg}>
+        </img>
       </div>
       <a className="Resume__download" download href={resumeSrc}>
         <button>Download</button>
