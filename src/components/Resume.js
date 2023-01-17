d3ef9c31f7dc6f2ed68788dfc6a9835b183c6599
username <gjaviles7@gmail.com>
2023-01-17 12:52:41 -0500

    added options

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,11 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
 import resumeSrc from "../media/resume.pdf";
 import "../styles/Resume.scss";
 
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+  standardFontDataUrl: 'standard_fonts/',
+};
 
 function Resume () {
   function removeTextLayerOffset() {
@@ -19,7 +24,7 @@ function Resume () {
   return (
     <div className="Resume" id="Resume">
       <div className="Resume__pdf-container">
-        <Document className="resume-pdf" file={resumeSrc}>
+        <Document className="resume-pdf" file={resumeSrc} options={options}>
           <Page onLoadSuccess={removeTextLayerOffset} pageNumber={1} />
         </Document>
       </div>
